b843f80863580753d071f3e1e4b274684d0d25f3
Walter Moreira <walter@waltermoreira.net>
2026-07-27 16:50:03 -0500

    Allow config for process-compose server

--- nix/modules/tms-portal.nix
+++ nix/modules/tms-portal.nix
@@ -9,6 +9,18 @@
             default = "https://github.com/tapis-project/tms_portal";
             description = "URL for the remote Git repository";
           };
+          process-compose-server = {
+            enable = lib.mkOption {
+              type = lib.types.bool;
+              default = true;
+              description = "Whether to enable the server for process compose";
+            };
+            port = lib.mkOption {
+              type = lib.types.port;
+              default = 1234;
+              description = "Port for process compose server";
+            };
+          };
         };
       };
     });
