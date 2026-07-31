894bbf01e6767c8d335a93c9919a6d65ca22ccac
Walter Moreira <walter@waltermoreira.net>
2026-07-30 22:11:54 -0500

    Add option to frontend module for redirect_uri

--- nix/modules/frontend.nix
+++ nix/modules/frontend.nix
@@ -1,6 +1,14 @@
-{ ... }: {
-  options = { };
-  config.perSystem = { pkgs, ... }:
+{ flake-parts-lib, ... }: {
+  options.perSystem = flake-parts-lib.mkPerSystemOption ({ lib, ... }: { 
+    options.tms-portal.frontend = {
+        redirect_uri = lib.mkOption {
+            type = lib.types.str;
+            default = "https://tms-auth-service.tacc.cloud/";
+            description = "Redirect URI for Identity Provider";
+        };
+    };
+  });
+  config.perSystem = { pkgs, config, ... }:
     let
       myPnpm = pkgs.pnpm_11.override { nodejs-slim = pkgs.nodejs-slim_24; };
       frontend = pkgs.stdenv.mkDerivation (final: {
@@ -21,6 +29,7 @@
           fetcherVersion = 4;
           hash = "sha256-fN07nWD8ZyJHu+OTAzNQquyYH5pmgtMx6poMRUOkazM=";
         };
+        VITE_REDIRECT_URI = config.tms-portal.frontend.redirect_uri;
         pnpmBuildScript = "build";
         pnpmBuildFlags = [
           "--mode"
